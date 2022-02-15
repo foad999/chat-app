8e8d8ec17c1bbfc88a01889e7bdd42a2227d826c
foad999 <94325633+foad999@users.noreply.github.com>
2022-02-15 18:14:01 +0330

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,10 +2,10 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 export const validator = firebase.initializeApp({
-    apiKey: "AIzaSyBC9XRoBe3rTeUKzxHzvEEoaHU623CVhhk",
-    authDomain: "foochat-1622e.firebaseapp.com",
-    projectId: "foochat-1622e",
-    storageBucket: "foochat-1622e.appspot.com",
-    messagingSenderId: "165521134273",
-    appId: "1:165521134273:web:45040be5d725cf77193a40"
+    apiKey: "AIk",
+    authDomain: "foom",
+    projectId: "fo",
+    storageBucket: "fooom",
+    messagingSenderId: "16134273",
+    appId: "1:16552a40"
 }).auth();
